46fb7042e6b63d44eb049584ad3e67fb0159e242
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-12 16:19:12 +0100

    Update mariadb_save.sh

--- ECOM/Docker/mariadb-docker/mariadb_save.sh
+++ ECOM/Docker/mariadb-docker/mariadb_save.sh
@@ -4,4 +4,4 @@ TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 BACKUP_DIR="./backup/"
 BACKUP_FILE="$BACKUP_DIR/mariadb_backup_$TIMESTAMP.sql"
 
-docker exec mariadb-docker mysqldump -u root --password="tpRT9025" --single-transaction --databases ecomddb --skip-column-statistics > $BACKUP_FILE
+docker exec mariadb-docker mysqldump -u root --password=$1 --single-transaction --routines --databases ecomddb --skip-column-statistics > $BACKUP_FILE
