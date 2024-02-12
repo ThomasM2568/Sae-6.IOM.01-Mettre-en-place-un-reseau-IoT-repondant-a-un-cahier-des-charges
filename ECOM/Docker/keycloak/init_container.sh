6b719ea15c8c049bb3eecb1b5f340ff0903aaa05
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-12 11:25:22 +0100

    Update init_container.sh

--- ECOM/Docker/keycloak/init_container.sh
+++ ECOM/Docker/keycloak/init_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker run --name keycloak -p 8080:8080 \
-    -e KEYCLOAK_ADMIN=admin \
-    -e KEYCLOAK_ADMIN_PASSWORD=admin \
+    -e KEYCLOAK_ADMIN= $1 \
+    -e KEYCLOAK_ADMIN_PASSWORD= $2 \
     -v keycloak_save:/opt/keycloak/data \
     quay.io/keycloak/keycloak:latest start-dev
