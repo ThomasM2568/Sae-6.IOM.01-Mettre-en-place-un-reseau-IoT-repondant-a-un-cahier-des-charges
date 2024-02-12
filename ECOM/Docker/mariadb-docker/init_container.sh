6e172ab93715386fbc821132c652c5da2df43573
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-12 14:22:21 +0100

    Update init_container.sh

--- ECOM/Docker/mariadb-docker/init_container.sh
+++ ECOM/Docker/mariadb-docker/init_container.sh
@@ -12,14 +12,16 @@ if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
 else
     echo "Le conteneur $container_name n'existe pas."
     # Ajoutez ici les actions à effectuer si le conteneur n'existe pas
-    docker run -d --name mariadb-docker -e MYSQL_ROOT_PASSWORD=tpRT9025 -p 3306:3306 -v mariadb_save:/var/lib/mysql mariadb 
+    docker run -d --name mariadb-docker -e MYSQL_ROOT_PASSWORD=$2 -p 3306:3306 -v mariadb_save:/var/lib/mysql mariadb 
     docker exec mariadb-docker sh -c "apt update"
     docker exec mariadb-docker sh -c "apt install -y mysql-client"
     #docker cp ddb.sql mariadb-docker:/ddb.sql
     #docker cp procedures.sql mariadb-docker:/procedures.sql
-    docker exec -it mariadb-docker bash -c "echo 'CREATE DATABASE ecomddb;' | mysql -uroot -p'tpRT9025'"
-    docker exec -i mariadb-docker mysql -uroot -p"tpRT9025" -D ecomddb < ddb.sql
-    #docker exec -i mariadb-docker mysql -uroot -p"tpRT9025" -D ecomddb < procedures.sql
+    docker exec -it mariadb-docker bash -c "echo 'CREATE DATABASE ecomddb;' | mysql -uroot -p$2"
+    docker exec -i mariadb-docker mysql -uroot -p$2 -D ecomddb < ddb.sql
+    docker exec -i mariadb-docker mysql -uroot -p -e "RENAME USER 'root'@'localhost' TO '$1'@'localhost';"
+
+     #docker exec -i mariadb-docker mysql -uroot -p"tpRT9025" -D ecomddb < procedures.sql
 
     # Récupérer le répertoire du script
     script_dir=$(dirname "$(readlink -f "$0")")
