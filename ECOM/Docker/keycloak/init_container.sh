71d4a280afdd68c1c3f3ca3dfc53e85dbf4b9a07
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-12 15:49:23 +0100

    Update init_container.sh

--- ECOM/Docker/keycloak/init_container.sh
+++ ECOM/Docker/keycloak/init_container.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-sudo docker run --name keycloak -p 8080:8080 \
-    -e KEYCLOAK_ADMIN= $1 \
-    -e KEYCLOAK_ADMIN_PASSWORD= $2 \
-    -v keycloak_save:/opt/keycloak/data \
-    quay.io/keycloak/keycloak:latest start-dev
+container_name="mariadb-docker"
+# Vérifier si le conteneur existe
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+    echo "Le conteneur $container_name existe."
+    docker start $container_name
+    # Ajoutez ici les actions à effectuer si le conteneur existe
+    # Par exemple :
+    # docker start $container_name
+else
+    sudo docker run --name keycloak -p 8080:8080 \
+        -e KEYCLOAK_ADMIN= $1 \
+        -e KEYCLOAK_ADMIN_PASSWORD= $2 \
+        -v keycloak_save:/opt/keycloak/data \
+        quay.io/keycloak/keycloak:latest start-dev
+fi
