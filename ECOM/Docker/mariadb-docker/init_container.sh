c8a5cae20861e879bbbaea432d6076c30c986a2f
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-13 11:08:59 +0100

    Update init_container.sh

--- ECOM/Docker/mariadb-docker/init_container.sh
+++ ECOM/Docker/mariadb-docker/init_container.sh
@@ -1,136 +1,41 @@
-import argparse
-import os
-#import paramiko
-import git 
-import shutil
-#import subprocess
+#!/bin/bash
 
-#############################################  ARG
-parser = argparse.ArgumentParser(description='Script avec des arguments facultatifs')
+container_name="mariadb-docker"
+# Récupérer le répertoire du script
+script_dir=$(dirname "$(readlink -f "$0")")
 
-parser.add_argument('-os', type=str, default="Linux", help='The os you use, Linux or Windows', required=False)
+# Vérifier si le conteneur existe
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+    echo "Le conteneur $container_name existe."
+    docker start $container_name
+    # Ajoutez ici les actions à effectuer si le conteneur existe
+    # Par exemple :
+    # docker start $container_name
+else
+    echo "Le conteneur $container_name n'existe pas."
+    # Ajoutez ici les actions à effectuer si le conteneur n'existe pas
+    docker run -d --name mariadb-docker -e TZ=Europe/Paris -e MYSQL_ROOT_PASSWORD=$2 -p 3306:3306 -v mariadb_save:/var/lib/mysql mariadb 
+    docker exec mariadb-docker sh -c "apt update"
+    docker exec mariadb-docker sh -c "apt install -y mysql-client"
+    #docker cp ddb.sql mariadb-docker:/ddb.sql
+    #docker cp procedures.sql mariadb-docker:/procedures.sql
+    docker exec -it mariadb-docker bash -c "echo 'CREATE DATABASE ecomddb;' | mysql -uroot -p$2"
+    docker exec -i mariadb-docker mysql -uroot -p$2 -D ecomddb < "$script_dir/ddb.sql"
+    docker exec -i mariadb-docker mysql -uroot -p$2 -e "RENAME USER 'root'@'localhost' TO '$1'@'localhost';"
 
-#parser.add_argument('--ecom_id', type=str, default="None", help='ECOM id', required=True)
-#parser.add_argument('--ecom_password', type=str, default="None", help='ECOM password', required=True)
-#parser.add_argument('--ecom_server', type=str, default="None", help='ECOM password', required=True)
-
-parser.add_argument('--service_id', type=str, default="admin", help='ECOM', required=False)
-parser.add_argument('--service_password', type=str, default="admin", help='ECOM', required=False)
-
-
-args = parser.parse_args()
-########################################################################################################################
-
-def repertoire_non_vide(chemin_repertoire):
-    # Vérifie si le chemin est un répertoire
-    if not os.path.isdir(chemin_repertoire):
-        raise ValueError("Le chemin spécifié n'est pas un répertoire.")
-    
-    # Liste le contenu du répertoire
-    contenu = os.listdir(chemin_repertoire)
-    
-    # Retourne True si le répertoire n'est pas vide, sinon False
-    return bool(contenu)
-
-
-def is_line_in_crontab(line):
-    try:
-        # Ouvre le fichier crontab en mode lecture
-        with open('/etc/crontab', 'r') as file:
-            # Lit toutes les lignes du fichier
-            for l in file:
-                # Vérifie si la ligne spécifiée est présente
-                if line in l:
-                    return True
-        return False
-    except FileNotFoundError:
-        print("Le fichier crontab n'a pas été trouvé.")
-        return False
-        
-
-def download_github_folder(repo_url, folder_path, destination_path):
-    try:
-        print('Recuperation dossier github')
-        if os.path.exists('temp_repo'):
-            shutil.rmtree('temp_repo')
-
-         # Cloner le référentiel GitHub en mémoire
-        git.Repo.clone_from(repo_url, 'temp_repo')
-
-         # Supprimer le répertoire de destination s'il existe déjà
-        if os.path.exists(destination_path):
-            shutil.rmtree(destination_path)
-
-         # Copier le dossier spécifique vers l'emplacement de destination
-        shutil.copytree('./temp_repo/' + folder_path, destination_path)
-
-         # Supprimer le référentiel temporaire
-        shutil.rmtree('temp_repo')
-        print('Dossier github recupere !')
-        return destination_path
-
-    except Exception as e:
-        print(f"Erreur lors du téléchargement du dossier depuis GitHub: {e}")
-        return None
+     #docker exec -i mariadb-docker mysql -uroot -p"tpRT9025" -D ecomddb < procedures.sql
 
     
-def main():
-    if (args.os.lower()!='linux'):
-        return (args.os+' not supported at the moment')
-    args_dict = vars(args)
-    print(args_dict)
-    os.system('apt install -y docker docker-compose python3')
-    
-    '''
-    directories=['./script','./log']
-    for directory in directories:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            print('Repertoire cree')
-    '''
-    #if(args_dict['ecom_id']!='None' and args_dict['ecom_password']!='None' and args_dict['ecom_server']!='None'):
-       #scp(args_dict['ecom_id'],args_dict['ecom_password'],args_dict['ecom_server'],'/script','/script')
-       
-    repo_url = "https://github.com/ThomasM2568/Sae-6.IOM.01-Mettre-en-place-un-reseau-IoT-repondant-a-un-cahier-des-charges.git"
-    downloaded_folder_path = []
-    downloaded_folder_path.append(download_github_folder(repo_url, 'ECOM/Docker', './docker'))
-    downloaded_folder_path.append(download_github_folder(repo_url, 'ECOM/Script', './script'))
-    downloaded_folder_path.append(download_github_folder(repo_url, 'ECOM/App', './app'))
-    
 
+    # Ligne à ajouter dans le crontab
+    cron_line="0 0 * * * $script_dir/mariadb_save.sh"
 
-    print(downloaded_folder_path)
-
-    #os.system('pip install requests ipaddress paho.mqtt time datetime meross_iot asyncio pymysql json')
-    if repertoire_non_vide('./docker/'):
-#         docker_content = os.listdir('./docker')
-         for path in os.listdir('./docker'):
-             print(path)
-             if os.path.exists('./docker/'+path+'/init_container.sh'):
-                 print(args_dict['service_id'])
-                 print(args_dict['service_password'])
-                 print('Lancement du script : ./docker/'+path+'/init_container.sh')
-                 #subprocess.run(['sudo', './docker/' + path + '/init_container.sh', args_dict['service_id'], args_dict['service_password']])
-                 # Remplacez 'votre_script.sh' par le chemin vers votre script shell
-                 script_path = './docker/' + path + '/init_container.sh'
-
-                 # Liste des arguments à passer au script shell
-                 arguments = [args_dict['service_id'], args_dict['service_password']]
- 
-                 # Utilisation de os.system pour exécuter le script en tant que superutilisateur
-                 os.system(f'sudo sh {script_path} {" ".join(arguments)}')
-             else:
-                 print('Le fichier init du container n existe pas')
-    #regarder si les scripts sont lancés automatiquement par crontab
-
-    
-    
-    #sinon les ajouter avec le script
-    
-    
-        
-    #else:
-        #return ('Please provide valid ecom_id and ecom_password')
-
-if __name__ == "__main__":
-    print(main())
+    # Vérifier si la ligne est déjà présente dans le crontab
+    if ! crontab -l | grep -qF "$cron_line"; then
+       # Ajouter la ligne au crontab
+       (crontab -l ; echo "$cron_line") | crontab -
+       echo "Ligne ajoutée au crontab."
+    else
+       echo "La ligne est déjà présente dans le crontab."
+    fi
+fi
