3b129dad2ac428bcd86b478270f924284b565afb
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2024-02-13 11:35:28 +0100

    Update init_container.sh

--- ECOM/Docker/keycloak/init_container.sh
+++ ECOM/Docker/keycloak/init_container.sh
@@ -9,7 +9,7 @@ if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
     # Par exemple :
     # docker start $container_name
 else
-    sudo docker run --name keycloak-docker -p 8080:8080 \
+    sudo docker run -d --name keycloak-docker -p 8080:8080 \
         -e KEYCLOAK_ADMIN= $1 \
         -e KEYCLOAK_ADMIN_PASSWORD= $2 \
         -v keycloak_save:/opt/keycloak/data \
